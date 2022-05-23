a4a53fb2ef68b2b6be2f93dff6a2a4dca1f1efa0
PrajjuS <theprajjus@gmail.com>
2022-05-23 12:07:40 +0530

    ReadmeGenerator: Initial commit [skip ci]
    - Still in BETA stage
    - ToDo:
        1. Switch to match case instead of find()
        2. Improve devices sorting logic
    
    Change-Id: I467019279b6749efef506b3c90d31aceaf0b6096
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

---
+++ .github/scripts/gen-devices-readme.py
@@ -0,0 +1,133 @@
+import re
+import os
+import json
+import github
+
+try:
+    GH_TOKEN = os.environ.get("GH_TOKEN")
+except Exception:
+    print("GH_TOKEN missing")
+    exit(0)
+START_COMMENT = "<!--START_SECTION:devices-->"
+END_COMMENT = "<!--END_SECTION:devices-->"
+RE_PATTERN = f"{START_COMMENT}[\\s\\S]+{END_COMMENT}"
+jsonDir = "builds"
+
+XIAOMI = []
+REALME = []
+SAMSUNG = []
+ASUS = []
+ONEPLUS = []
+LENOVO = []
+NOKIA = []
+MEIZU = []
+OTHER = []
+
+def generate_new_readme(devices, readme):
+    replace_readme = f"{START_COMMENT}\n{devices}\n{END_COMMENT}"
+    return re.sub(RE_PATTERN, replace_readme, readme)
+
+def get_all_filenames():
+    FILENAMES = []
+    for device in os.listdir(jsonDir):
+        FILENAMES.append(device.replace(".json", ""))
+    return FILENAMES
+
+def get_info(filename):
+    with open(f"{jsonDir}/{filename}.json") as device_file:
+            info = json.loads(device_file.read())
+            CODENAME = info['device']
+            DEVICE_NAME = info['device_name']
+            MAINTAINER = info['tg_username']
+    return {
+        "codename": CODENAME,
+        "device_name": DEVICE_NAME,
+        "maintainer": MAINTAINER
+    }
+
+def check_and_add_device(filename):
+    info = get_info(filename)
+    device_name = info['device_name']
+    if device_name.upper().find("XIAOMI") != -1 or device_name.upper().find("REDMI") != -1 or device_name.upper().find("POCO") != -1 or device_name.upper().find("MI") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        XIAOMI.append(TEXT)
+    elif device_name.upper().find("REALME") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        REALME.append(TEXT)
+    elif device_name.upper().find("SAMSUNG") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        SAMSUNG.append(TEXT)
+    elif device_name.upper().find("ASUS") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        ASUS.append(TEXT)
+    elif device_name.upper().find("ONEPLUS") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        ONEPLUS.append(TEXT)
+    elif device_name.upper().find("LENOVO") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        LENOVO.append(TEXT)
+    elif device_name.upper().find("NOKIA") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        NOKIA.append(TEXT)
+    elif device_name.upper().find("MEIZU") != -1:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        MEIZU.append(TEXT)
+    else:
+        TEXT = f"{device_name} ({info['codename']}) by {info['maintainer']}"
+        OTHER.append(TEXT)
+
+def list_devices(device_list):
+    TEXT = "```\n"
+    for device in device_list:
+        TEXT += f"{device}\n"
+    TEXT += "```"
+    return TEXT
+
+def update_devices():
+    for filename in get_all_filenames():
+        check_and_add_device(filename)
+    UPDATE = ""
+    UPDATE += "## - Xiaomi Devices\n"
+    UPDATE += f"{list_devices(sorted(XIAOMI))}\n\n"
+    UPDATE += "## - Realme Devices\n"
+    UPDATE += f"{list_devices(sorted(REALME))}\n\n"
+    UPDATE += "## - Samsung Devices\n"
+    UPDATE += f"{list_devices(sorted(SAMSUNG))}\n\n"
+    UPDATE += "## - Asus Devices\n"
+    UPDATE += f"{list_devices(sorted(ASUS))}\n\n"
+    UPDATE += "## - OnePlus Devices\n"
+    UPDATE += f"{list_devices(sorted(ONEPLUS))}\n\n"
+    UPDATE += "## - Lenovo Devices\n"
+    UPDATE += f"{list_devices(sorted(LENOVO))}\n\n"
+    UPDATE += "## - Nokia Devices\n"
+    UPDATE += f"{list_devices(sorted(NOKIA))}\n\n"
+    UPDATE += "## - Meizu Devices\n"
+    UPDATE += f"{list_devices(sorted(MEIZU))}\n\n"
+    UPDATE += "## - Other Devices\n"
+    UPDATE += f"{list_devices(sorted(OTHER))}"
+    UPDATE += ""
+    return UPDATE
+
+try:
+    g = github.Github(GH_TOKEN)
+    committer = github.InputGitAuthor("TheElixirRobot", "projectelixirbot@gmail.com")
+    repo = g.get_repo("ProjectElixir-Devices/official_devices")
+    commit_message = "Elixir: Update devices readme [BOT]"
+    contents = repo.get_readme()
+    readme = contents.decoded_content.decode()
+    device_update = update_devices()
+    new_readme = generate_new_readme(device_update, readme)
+    if new_readme != readme:
+        repo.update_file(
+            path=contents.path,
+            message=commit_message,
+            content=new_readme,
+            sha=contents.sha,
+            branch='master',
+            committer=committer
+        )
+    else:
+        print("Nothing to change in devices readme\nExitting...")
+        exit(1)
+except Exception as e:
+    print(f"Exception Occurred: {str(e)}")
--- README.md
+++ README.md
@@ -8,79 +8,8 @@
 Project Elixir is another aftermarket AOSP ROM which basically offers minimal UI enhancement & close to Stock Android ROM with great performance, security and stability. Most of the OEMs' these days will provide slow and untimely updates, but we don't do that here. We closely follow Google to bring the latest updates to our users, and even prolong support for devices that have been declared obsolete by OEMs. Our ROMs' source code is open-source, secure, stable, and outstanding. Your experience while using Project Elixir will be butter smooth without compromising the quality of the Android experience. In short, it's perfectly balanced between Great Performance, Security, stability, minimal UI & awesome features including pixel goodies. So do not hesitate anymore, join us now and start enjoying the beauty of stock Android. 
 
 # // OFFICIAL MAINTAINERS OF Project Elixir //
-
-## - Xiaomi (Redmi Series)
-```bash
-Redmi S2/Y2 (ysl) by Sushmit
-Redmi Note 5 Pro (whyred) by Gaganpreet Singh
-Redmi Note 6 Pro (tulip) by RahulPalXDA
-Redmi Note 7 Pro (violet) by Saurav
-Redmi Note 7 (lavender) by Aaradhay Vijay
-Redmi Note 8/8T (ginkgo) by RahulPalXDA
-Redmi Note 10 (mojito/sunny) by Jaydeep
-Redmi Note 10 Pro/Max (sweet/sweetin) by zaidkhan0997
-Redmi K30 (phoenix) by Ankit
-Redmi Note 9 Pro 5G (gauguinpro) by Hridaya
-Redmi Note 8 Pro (begonia) by PiotrBurdzinski
-Redmi 9T/ 9 Power / 9 4G China / POCO M3 (juice) by Zahran
-```
-
-## - Xiaomi (Poco Series)
-```bash
-Xiaomi Poco F1 (beryllium) by Prabh
-Xiaomi Poco X2 (phoenix) by Ankit
-Xiaomi Poco X3 Pro (vayu/bhima) by Pritish
-Xiaomi Poco F3/11x/K40 (Alioth/Aliothin) by Dark_phoenix
-```
-
-## - Xiaomi (Mi Series)
-```bash
-Xiaomi Mi A3 (laurel_sprout) by H3M3L
-Xiaomi Mi 9 (cepheus) by Mehdi
-Xiaomi Mi 10i 5G (gauguininpro) by Hridaya
-Xiaomi Mi 10T Lite (gauguin_global) by Hridaya
-Xiaomi Mi Note 10 Lite (toco) by Dennis Murimi
-```
-
-## - Nokia Devices
-```bash
-
-```
-
-## - Lenovo Devices
-```bash
-
-``` 
-
-## - Realme Devices
-```bash
-Realme 3/3i (RMX1821) by Pritish
-Realme 2 Pro (RMX1801) by Abhishek
-Realme C2 (RMX1941) by Sundram
-Realme C3/Narzo 10A (RMX2020) by Anoos
-Realme 5/5i/5s Unified (RMX1911/1925/2030) by MudabbirulSaad
-Realme XT (RMX1921) by Probir
-Realme X3 (RMX2081) by Patriot
-Realme X3 SuperZOOM (RMX2081) by Patriot
-Realme 6 pro (RMX2061/2063) by Aman
-```
-
-## - Meizu Devices
-```bash
-
-```
-
-## - Asus Devices
-```bash
-Asus Zenfone Max Pro M1 (X00TD) by Harshhaa Reddy Pamarthi
-```
-
-## - OnePlus Devices
-```bash
-OnePlus 8T(kebab) by Viper_1057 or Amsal1
-OnePlus 9R(lemonades) by Viper_1057 or Amsal1
-Oneplus 5T(dumpling) by MudabbirulSaad
-```
+<!--START_SECTION:devices-->
+<!--END_SECTION:devices-->
 
 # Maintainership 
 
